bccedacc224261d515d115465b71ceb31dc3d9c1
denovanrr <137625696+denovanrr@users.noreply.github.com>
2023-11-19 15:59:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import {Router} from "./router.js"
 
 const router = new Router()
-router.add('/',"/paginas/home.html")
-router.add("/about","/paginas/about.html")
-router.add("/contact","/paginas/contact.html")
-router.add(404,"/paginas/404.html")
+router.add('/',"/home.html")
+router.add("/about","/about.html")
+router.add("/contact","/contact.html")
+router.add(404,"/404.html")
 
 router.handle()
 
